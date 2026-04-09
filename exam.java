897015542e090e94bc2d65c6b1ca87b5b4a0570e
박시후 <hoo06240624@gmail.com>
2026-04-09 15:34:33 +0900

    2307

--- exam.java
+++ exam.java
@@ -1,1 +1,19 @@
+class Character {
+    Posion myPosion;
+    Weapon myWeapon;
 
+    Character() {
+    }
+
+    public void getMyWeapon(Weapon w) {
+    }
+}
+
+class Posion {
+}
+
+class Weapon {
+}
+
+class Monster {
+}
